12bee01a65f3371e28d66a69c4fdbf877c33cdf7
MARFIK <tomaszewski.kacper1@tlen.pl>
2026-04-16 22:09:19 +0200

    fix: remove invalid color prop from loading.tsx files

--- app/match/loading.tsx
+++ app/match/loading.tsx
@@ -2,10 +2,8 @@ import LoadingComponent from "../components/common/LoadingComponent";
 
 export default function Loading() {
     return (
-        <div className="flex flex-col w-full justify-center items-center h-screen">
-            <LoadingComponent 
-                color="white" 
-            />
+        <div className="flex flex-col w-full justify-center items-center min-h-[60vh]">
+            <LoadingComponent />
         </div>
-    )
+    );
 }
